b69f7d7d9a3a6bcfb8f82f0fb74bcb1a0a1720f8
unaG <veraguo11@163.com>
2021-11-25 19:30:20 +0800

    Create single_style_mixing.py

---
+++ single_style_mixing.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+"""Generate style mixing image matrix using pretrained network pickle."""
+
+import os
+from glob import glob
+import re
+from typing import List
+
+import click
+import dnnlib
+import numpy as np
+import cv2
+import PIL.Image
+import torch
+
+import legacy
+
+#----------------------------------------------------------------------------
+
+def num_range(s: str) -> List[int]:
+    '''Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.'''
+
+    range_re = re.compile(r'^(\d+)-(\d+)$')
+    m = range_re.match(s)
+    if m:
+        return list(range(int(m.group(1)), int(m.group(2))+1))
+    vals = s.split(',')
+    return [int(x) for x in vals]
+
+#----------------------------------------------------------------------------
+
+@click.command()
+@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
+@click.option('--rows', 'row_seeds', type=num_range, help='Random seeds to use for image rows', required=True)
+@click.option('--cols', 'col_seeds', type=num_range, help='Random seeds to use for image columns', required=True)
+@click.option('--styles', 'col_styles', type=num_range, help='Style layer range', default='0-6', show_default=True)
+@click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
+@click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
+@click.option('--outdir', type=str, required=True)
+@click.option('--srcpath', type=str, required=True)
+@click.option('--trgpath', type=str, required=True)
+def generate_style_mix(
+    network_pkl: str,
+    row_seeds: List[int],
+    col_seeds: List[int],
+    col_styles: List[int],
+    truncation_psi: float,
+    noise_mode: str,
+    outdir: str,
+    srcpath: str,
+    trgpath: str
+):
+    """Generate images using pretrained network pickle.
+
+    Examples:
+
+    \b
+    python style_mixing.py --outdir=out --rows=85,100,75,458,1500 --cols=55,821,1789,293 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+    """
+    print('\nLoading networks from "%s"...' % network_pkl)
+    device = torch.device('cuda')
+    with dnnlib.util.open_url(network_pkl) as f:
+        G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+
+    os.makedirs(outdir, exist_ok=True)
+
+    src_w = np.load(srcpath)
+    trg_w = np.load(trgpath)
+    print('\nGenerating style-mixed image...')
+    src_w[col_styles] = trg_w[col_styles]
+    image = G.synthesis(src_w[np.newaxis], noise_mode=noise_mode)
+    image = (image.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+    mix_image = image[0].cpu().numpy()
+
+
+    print('\nSaving images...')
+    os.makedirs(outdir, exist_ok=True)
+    PIL.Image.fromarray(mix_image, 'RGB').save(f'single_mix.png')
+
+
+#----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    generate_style_mix() # pylint: disable=no-value-for-parameter
+
+#----------------------------------------------------------------------------
